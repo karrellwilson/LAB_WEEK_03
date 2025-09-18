ab4c81e0aa7de8d68b865e54cf5416f560feeaab
karrellwilson <163616212+karrellwilson@users.noreply.github.com>
2025-09-18 09:30:29 +0700

    Commit No.4 – use jetpack navigation

--- app/src/main/java/com/example/lab_week_03/ListFragment.kt
+++ app/src/main/java/com/example/lab_week_03/ListFragment.kt
@@ -1,28 +1,18 @@
 package com.example.lab_week_03
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
-class ListFragment : Fragment(), View.OnClickListener {
-    private lateinit var coffeeListener: CoffeeListener
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is CoffeeListener) {
-            coffeeListener = context
-        } else {
-            throw RuntimeException("Must implement CoffeeListener")
-        }
-    }
-
+class ListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
@@ -33,14 +23,17 @@ class ListFragment : Fragment(), View.OnClickListener {
             view.findViewById(R.id.americano),
             view.findViewById(R.id.latte)
         )
-        coffeeList.forEach {
-            it.setOnClickListener(this)
+
+        coffeeList.forEach { coffee ->
+            coffee.setOnClickListener {
+                val fragmentBundle = Bundle()
+                fragmentBundle.putInt(COFFEE_ID, coffee.id)
+                findNavController().navigate(R.id.coffee_id_action, fragmentBundle)
+            }
         }
     }
 
-    override fun onClick(v: View?) {
-        v?.let {
-            coffeeListener.onSelected(it.id)
-        }
+    companion object {
+        const val COFFEE_ID = "COFFEE_ID"
     }
 }
--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -3,33 +3,9 @@ package com.example.lab_week_03
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-interface CoffeeListener {
-    fun onSelected(id: Int)
-}
-
-class MainActivity : AppCompatActivity(), CoffeeListener {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Tampilkan ListFragment saat activity pertama kali dibuat
-        if (savedInstanceState == null) {
-            val listFragment = ListFragment()
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, listFragment)
-                .commit()
-        }
-    }
-
-    override fun onSelected(id: Int) {
-        // Buat instance DetailFragment menggunakan newInstance
-        val detailFragment = DetailFragment.newInstance(id)
-
-        // Ganti ListFragment dengan DetailFragment
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, detailFragment)
-            // Tambahkan transaksi ini ke back stack agar bisa kembali
-            .addToBackStack(null)
-            .commit()
     }
 }
